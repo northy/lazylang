d31d2deae94d7e1a309f7ce54f1208a95654e4c5
Igorronsoni <igorandrey@yahoo.com.br>
2019-04-24 10:11:52 -0300

    Update Parser.java part Int

--- fonte/Parser.java
+++ fonte/Parser.java
@@ -1,62 +1,41 @@
 import java.util.HashMap;
 
 class Parser{
-	protected String typ = "",data;
-	protected int i = 0,result;
-	IntVar variavel;
-	
-	//Contructor
-	public Parser(String phrase){
+	public static void 	parse(String s, HashMap<String,Var> h){
+		String subs = "",varName = "";
+		int old = 0,k;
 		//Percorre a linha
-		phrase = phrase.replaceAll(" ","");
-		for(i = 0; i < phrase.length(); i++){
-			typ = typ + phrase.substring(i,i+1);
-
-			//Type Int
-			if(typ.equals("int")){
-				typ = "";
-				for(i = i+1; i < phrase.length() - 1; i++){
-					data = "";
-
-					//Atribuição na instanciação //MUDAR
-					if(phrase.substring(i,i+1).equals("=")){
-						data = phrase.substring(i+1,phrase.length());
-						if(data != ""){
-							result = Integer.parseInt(data);
-						}else{
-							result = 0;
+		s = s.replaceAll(" ","");
+		for(int i = 0; i < s.length(); i++){
+			//
+			if(s.charAt(i) == ';'){
+				//Percorre subs
+				for(int j = old; j < i-1; j++){
+					subs += s.substring(j,j+1); 
+					
+					//Types
+					//Type Int
+					if(subs.equals("int")){
+						k=old+j+2;
+						varName = "";
+						while(k < s.length() && (s.charAt(k)!=';' || s.charAt(k)!='=' || s.charAt(k)!=',')){
+							varName += s.charAt(k-1);
+							k++;
 						}
-						variavel = new IntVar(typ,result);	
+					 	h.put(varName,new IntVar(varName));
 					}
-					//Término do comando //MUDAR
-					else if(phrase.substring(i,i+1).equals(";")){
-						result = Integer.parseInt(data);
-						variavel = new IntVar(typ,result);
+
+					//Type Double
+					if(subs.equals("double")){
+						return;
 					}
-					//Continua a adicionar character na variavel	
-					else{
-						typ += phrase.substring(i,i+1);
+
+					//Type String
+					if(subs.equals("string")){
+						return; 
 					}
 				}
 			}
-
-			//Type Double
-			if(typ.equals("double")){
-				return;
-			}
-
-			//Type String
-			if(typ.equals("string")){
-				return; 
-			}
-
-			//Type ...
 		}
-	}
-
-	public static void Parse(String type, String name){
-		HashMap <String,String> variavel = new  HashMap<String,String>();
-		variavel.put(type,name);
-		return;
 	}
 }
