468fd9161e271b792bb70628667adc600c887c82
Igorronsoni <igorandrey@yahoo.com.br>
2019-04-26 21:40:23 -0300

    Testes no vector aleatorios

--- fonte/Vector.java
+++ fonte/Vector.java
@@ -1,3 +1,11 @@
+import java.util.HashMap;
 class Vector{
-	//Criação de vetor
+	public static void main(String args[]){
+		HashMap a = new HashMap<String,String>();
+		String teste = "1",bb = "";
+		int i = 1;
+		a.put("B","Puta");
+		//System.out.println(a.get(teste));
+		System.out.println(Integer.toString(i) + 2);
+	}
 }
