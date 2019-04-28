66d5193f00ac1b6f83941ec9404eb6068c95643c
Igorronsoni <igorandrey@yahoo.com.br>
2019-04-28 11:21:11 -0300

    Alteração na classe Parser: Criação de funções para cada operação (-- criação de variavel -- loop -- condição --) para a facilitar a implementação dos loops e condições

--- fonte/Test.java
+++ fonte/Test.java
@@ -3,14 +3,14 @@ import java.util.HashMap;
 class Test {
     public static void main(String[] args) {
         HashMap a = new HashMap<String,Var>();
-        Parser.parse("bool a = 1, b, c = false; int d, e = 14; double f = 1, g= 6, h;",a);
+        Parser.parse("int a;",a);
         ((Var)a.get("a")).print();
-        ((Var)a.get("b")).print();
-        ((Var)a.get("c")).print();
-        ((Var)a.get("d")).print();
-        ((Var)a.get("e")).print();
-        ((Var)a.get("f")).print();
-        ((Var)a.get("g")).print();
-        ((Var)a.get("h")).print();
+        //((Var)a.get("b")).print();
+        //((Var)a.get("c")).print();
+        //((Var)a.get("d")).print();
+        //((Var)a.get("e")).print();
+        //((Var)a.get("f")).print();
+        //((Var)a.get("g")).print();
+        //((Var)a.get("h")).print();
     }
 }
